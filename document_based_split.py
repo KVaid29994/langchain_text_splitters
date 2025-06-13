8f66293b969e1d4c0a511eb7ed5511b36c1d8438
Kashish <kashish.vaid29994@gmail.com>
2025-06-13 09:14:51 +0530

    completed text splitters

--- document_based_split.py
+++ document_based_split.py
@@ -1,6 +1,6 @@
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter, Language
 
-text = '''
+text = '''from langchain_text_splitters import RecursiveCharacterTextSplitter
 The most intuitive strategy is to split documents based on their length. This simple yet effective approach ensures that each chunk doesn't exceed a specified size limit. Key benefits of length-based splitting:
 
 Straightforward implementation
@@ -10,7 +10,7 @@ Types of length-based splitting:
 
 Token-based: Splits text based on the number of tokens, which is useful when working with language models.
 Character-based: Splits text based on the number of characters, which can be more consistent across different types of text.
-'''
+
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size = 100, chunk_overlap = 0)
 
@@ -18,3 +18,12 @@ result = text_splitter.split_text(text)
 print (len(result))
 
 print (result[1:5])
+
+
+'''
+
+splitter = RecursiveCharacterTextSplitter.from_language(language= Language.PYTHON, chunk_size = 300, chunk_overlap =0)
+
+result = splitter.split_text(text)
+
+print (result[-1])
